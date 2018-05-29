20ed6529eb2008598fa1ab5542a20f7c59dd18cb
Marcos <Marcos@casa-equipo2.mundo-R.com>
2018-05-30 00:08:55 +0200

    9_2

---
+++ Boletin_9_2/src/boletin_9_2/Boletin_9_2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_9_2;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Boletin_9_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     SumaProducto ex = new SumaProducto();
+     ex.suma();
+     ex.producto();
+    }
+    
+}
