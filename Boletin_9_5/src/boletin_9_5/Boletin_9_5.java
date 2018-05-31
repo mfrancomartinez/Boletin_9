fe8e9c85e20d945467078d5011d5e749188c5945
Marcos <Marcos@casa-equipo2.mundo-R.com>
2018-05-31 19:14:34 +0200

    9_5

---
+++ Boletin_9_5/src/boletin_9_5/Boletin_9_5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_9_5;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Boletin_9_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Series ex = new Series();
+ex.primeraserie();
+ex.segundaserie();
+ex.terceraserie();
+    }
+    
+}
