66bd52e7ef697cdb1d89e7f26294b7bebf73be65
Marcos <Marcos@casa-equipo2.mundo-R.com>
2018-05-31 20:58:54 +0200

    9_6

---
+++ Boletin_9_6/src/boletin_9_6/Boletin_9_6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_9_6;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Boletin_9_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Soldos ex = new Soldos();
+ex.datossoldos();
+
+    }
+    
+}
